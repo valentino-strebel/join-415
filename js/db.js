9a1c7b6dbaaf6a08e65ecc02a9b02fad35dba59f
Vale Cannelloni <tino_schweizer@hotmail.com>
2025-03-07 20:20:29 +0100

    all functions shortened

--- js/db.js
+++ js/db.js
@@ -1,19 +1,29 @@
 let tasks = [];
+
 let subtasks = [];
+
 let subtaskInputs = [];
+
 let contacts = [];
+
 let taskKey = 0;
+
 let assigneeEditKey = [];
+
 let selectedContactsIDs = [];
+
 let selectedAssignee = [];
+
 let newPrio = "";
-//let emailPattern = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+
 let urgencySymbols = [
   "../assets/icons/add_task/prio-low-icon.svg",
   "../assets/icons/add_task/prio-medium-icon.svg",
   "../assets/icons/add_task/prio-urgent-icon.svg",
 ];
+
 let months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
+
 let bgcolors = [
   { id: 0, rgba: "rgba(255, 105, 135, 1)" },
   { id: 1, rgba: "rgba(255, 180, 120, 1)" },
@@ -26,6 +36,7 @@ let bgcolors = [
   { id: 8, rgba: "rgba(138, 43, 226, 1)" },
   { id: 9, rgba: "rgba(255, 165, 0, 1)" },
 ];
+
 let tagColors = ["rgba(0, 56, 255, 1)", "rgba(31, 215, 193, 1)"];
 
 async function loadDataBoard() {
@@ -79,15 +90,7 @@ async function getTasks(path = `tasks/`) {
   if (tasksData != null) {
     Object.entries(tasksData).forEach(([id, content]) => {
       let subtasksArray = [];
-      if (content.subtask) {
-        subtasksArray = Object.entries(content.subtask)
-          .filter(([key, value]) => value !== null && value !== undefined)
-          .map(([subtaskId, subtaskContent]) => ({
-            id: subtaskId,
-            checked: subtaskContent.checked,
-            text: subtaskContent.text,
-          }));
-      }
+      createTaskDataSubtask(subtasksArray, content);
       let contactArray = [];
       if (content.contact) {
         contactArray = Object.entries(content.contact)
@@ -99,6 +102,18 @@ async function getTasks(path = `tasks/`) {
       }
       tasksPush(tasks, id, content, subtasksArray, contactArray);
     });
+  }
+}
+
+function createTaskDataSubtask(subtasksArray, content) {
+  if (content.subtask) {
+    subtasksArray = Object.entries(content.subtask)
+      .filter(([key, value]) => value !== null && value !== undefined)
+      .map(([subtaskId, subtaskContent]) => ({
+        id: subtaskId,
+        checked: subtaskContent.checked,
+        text: subtaskContent.text,
+      }));
   }
 }
 
